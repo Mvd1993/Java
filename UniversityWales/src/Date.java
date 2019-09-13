e569af46ea11d9b2bc1c4541a781565eaee7cb41
mati <mati@DESKTOP-1VVBL96>
2019-09-13 13:25:17 +0200

    Final UniversityWales

--- UniversityWales/src/Date.java
+++ UniversityWales/src/Date.java
@@ -1,3 +1,39 @@
-public class Date {
+import java.time.LocalDate;
 
+public class Date {
+LocalDate date;
+	
+	
+	public Date() {
+		this.date=LocalDate.now();
+	}
+	public Date(LocalDate date) {
+		this.date = date;
+	}
+	
+
+	public void addYears(long years){
+		this.date = this.date.plusYears(years);
+		
+		
+	}
+	
+	public LocalDate dateNow() {
+		return LocalDate.now();
+	}
+	
+	//----------------------------------------------------
+	
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	@Override
+	public String toString() {
+		return "MyDate [date=" + date + "]";
+	}
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
 }
